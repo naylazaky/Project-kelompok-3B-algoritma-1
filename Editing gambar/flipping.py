924f928c0fa8e3112ca0b63a6710af140695b759
nayla zaky <lovegoddestluna@gmail.com>
2024-10-03 13:13:04 +0800

    flipping.py

--- Editing gambar/flipping.py
+++ Editing gambar/flipping.py
@@ -1,13 +1,7 @@
 from PIL import Image, ImageOps
 
 def flip_image(image_path,output_path,direction):
-    """
-    Memantulkan gambar baik secara horizontal atau vertikal.
 
-    :param image_path: Path ke file gambar yang ingin dipantulkan.
-    :param output_path: Path untuk menyimpan gambar yang dipantulkan.
-    :param direction: Arah pemantulan ('horizontal' atau 'vertical').
-    """
     try:
         # Buka gambar
         image = Image.open(image_path)
